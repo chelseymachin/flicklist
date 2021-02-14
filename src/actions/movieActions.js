bde4b2c6d3a5796cc8818617658a09e1e9718cb9
Chelsey Machin <chelseymachin@gmail.com>
2021-02-13 19:37:31 -0800

    Add fetchMovieByID function definition

--- src/actions/movieActions.js
+++ src/actions/movieActions.js
@@ -1,8 +1,20 @@
 import sanityAPI from "../../client.js";
 import {
-  MOVIES_FETCH_FAIL,
-  MOVIES_FETCH_REQUEST,
-  MOVIES_FETCH_SUCCESS  
+    MOVIE_FETCH_FAIL, 
+    MOVIE_FETCH_REQUEST, 
+    MOVIE_FETCH_SUCCESS,
+    MOVIES_FETCH_FAIL, 
+    MOVIES_FETCH_REQUEST, 
+    MOVIES_FETCH_SUCCESS, 
+    MOVIES_SORT_REQUEST,
+    MOVIES_SORT_SUCCESS,
+    MOVIES_SORT_FAIL,
+    MOVIES_MOST_POPULAR_REQUEST,
+    MOVIES_MOST_POPULAR_SUCCESS,
+    MOVIES_MOST_POPULAR_FAIL,
+    MOVIES_REF_FETCH_SUCCESS,
+    MOVIES_REF_FETCH_FAIL,
+    MOVIES_REF_FETCH_REQUEST
 } from "../constants/movieConstants.js";
 
 const fetchAllMovies = () => async (dispatch) => {
@@ -55,4 +67,51 @@ const fetchMoviesByRef = (ref) => async (dispatch) => {
     }
   };
 
-const fetchMovieByID = ()
+const fetchMovieByID = (id) => async(dispatch) => {
+    try {
+        dispatch({
+            type: MOVIES_FETCH_REQUEST
+        })
+
+        const data = await sanityAPI.fetch(
+            `*[_type == 'movie' && _id == '${id}']{                                                
+                _id,
+                "cast" :
+                    castMembers[]{
+                        "ref": person._ref,
+                        characterName, 
+                        "name": person->name,
+                        "image": person->image.asset->url
+                    }
+                ,
+                "crew" :
+                    crewMembers[]{
+                        "ref": person._ref,
+                        department, 
+                        job,
+                        "name": person->name,
+                        "image": person->image.asset->url
+                    }
+                ,                
+                "overview":   {                    
+                    "text": overview[0].children[0].text
+                  },
+                popularity,
+                "poster" : poster.asset->url,
+                releaseDate,                                
+                title
+            }[0]`
+        )
+        
+        dispatch({
+            type: MOVIE_FETCH_SUCCESS,
+            payload: data
+        })
+        
+    } catch (error) {
+        dispatch({
+            type: MOVIE_FETCH_FAIL,
+            payload: error.message
+        })
+    }
+}
